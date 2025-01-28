79a74b6528e825baa9ef5aa90e1551d7a8c784bf
Alikhan Beisenbayev <rkoenigseggagera@gmail.com>
2025-01-28 15:23:43 +0100

    url media fixed

--- src/components/Loader/Clouds.jsx
+++ src/components/Loader/Clouds.jsx
@@ -2,8 +2,8 @@ import styles from './Clouds.module.css';
 
 function Clouds() {
     return (<div className={styles.clouds_container}>
-        <img className={styles.cloud_one} src="assets\cloud2.png" alt="" />
-        <img className={styles.cloud_two} src="assets\cloud1.png" alt="" />
+        <img className={styles.cloud_one} src="images\cloud2.png" alt="" />
+        <img className={styles.cloud_two} src="images\cloud1.png" alt="" />
     </div>)
 }
 
